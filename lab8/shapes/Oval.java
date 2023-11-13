db32e1cbf4534f113afaeac12f2d1073514ebb2f
morphine198 <144669197+morphine198@users.noreply.github.com>
2023-11-13 16:26:28 +0300

    New shape

---
+++ lab8/shapes/Oval.java
@@ -0,0 +1,23 @@
+package edu.lab8.shapes;
+
+class Oval extends Shape {
+    private int width;
+    private int height;
+    public Oval(String colour, int x, int y, int width, int height) {
+        super(colour, x, y);
+        this.width = width;
+        this.height = height;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+}
